3396e4914adcf0f6b9ad660244491e52a866b051
0101spbl <114441053+0101spbl@users.noreply.github.com>
2022-10-14 08:51:14 +0200

    Update bitcoin.rb

--- bitcoin.rb
+++ bitcoin.rb
@@ -1,19 +1,13 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-   
 
+page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all"))  
+    crypto_name = page.xpath('//td[contains(@class,"symbol")]')
+    crypto_price = page.xpath('//td[contains(@class,"price")]')
 
-PAGE_URL = URI.open"https://coinmarketcap.com/all/views/all/"
+@list = crypto_name.zip( crypto_price )
 
-page = Nokogiri::HTML(open(PAGE_URL))
-
-# From the website, get an array of the currencies name and convert it to string
-currency_name_array = page.xpath("//tr/td/a[contains(@class, 'currency-name-container')]/text()").map {|x| x.to_s }
-# From the website, get an array of the currencies price and convert it to string
-currency_value_array = page.xpath("//tr/td/a[contains(@class, 'price')]/text()").map {|x| x.to_s }
-
-# Convert the 2 arrays into hash
-currency_result = Hash[currency_name_array.zip(currency_value_array)]
-
-puts currency_result
+@list.each do |crypto_name,crypto_price|
+     p "#{crypto_name.text} <#{crypto_price.text}>"
+end
